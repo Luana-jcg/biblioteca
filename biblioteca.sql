effa9a2ab56aed87557d0bfedce1edfa86ed7d3c
Luana-jcg <47464845+Luana-jcg@users.noreply.github.com>
2019-10-31 20:01:09 -0300

    Add files via upload

---
+++ biblioteca.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 31-Out-2019 às 23:58
+-- Versão do servidor: 10.4.6-MariaDB
+-- versão do PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `autor`
+--
+
+CREATE TABLE `autor` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `sobrenome` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `autor`
+--
+
+INSERT INTO `autor` (`id`, `nome`, `sobrenome`) VALUES
+(1, 'Daniel', 'Barret'),
+(2, 'Gerald', 'Carter'),
+(3, 'Mark', 'Sobell'),
+(4, 'William', 'Stanek'),
+(5, 'Richard', 'Blum');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `editora`
+--
+
+CREATE TABLE `editora` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `editora`
+--
+
+INSERT INTO `editora` (`id`, `nome`) VALUES
+(1, 'Prentice Hall'),
+(2, 'O´Reilly'),
+(3, 'Microsoft Press'),
+(4, 'Wiley');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livro`
+--
+
+CREATE TABLE `livro` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `isbn` varchar(30) DEFAULT NULL,
+  `data_pub` date DEFAULT NULL,
+  `preco` decimal(10,2) DEFAULT NULL,
+  `editora_id` int(11) DEFAULT NULL,
+  `autor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `livro`
+--
+
+INSERT INTO `livro` (`id`, `nome`, `isbn`, `data_pub`, `preco`, `editora_id`, `autor_id`) VALUES
+(1, 'Linux Command Line and Shell Scripting', '143856969', '2009-12-21', '68.35', 1, 4),
+(2, 'SSH, the Secure Shell', '127658789', '2009-12-21', '58.30', 1, 2),
+(3, 'Using Samba', '123856789', '2000-12-21', '61.45', 2, 2),
+(4, 'Fedora and Red Hat Linux', '123346789', '2010-11-01', '62.24', 3, 1),
+(5, 'Windows Server 2012 Inside Out', '123356789', '2004-05-17', '66.80', 4, 3),
+(6, 'Microsoft Exchange Server 2010', '123366789', '2000-12-21', '45.30', 4, 3);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `autor`
+--
+ALTER TABLE `autor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `editora`
+--
+ALTER TABLE `editora`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `editora_id` (`editora_id`),
+  ADD KEY `autor_id` (`autor_id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `autor`
+--
+ALTER TABLE `autor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `editora`
+--
+ALTER TABLE `editora`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `livro`
+--
+ALTER TABLE `livro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `livro`
+--
+ALTER TABLE `livro`
+  ADD CONSTRAINT `autor_id` FOREIGN KEY (`autor_id`) REFERENCES `autor` (`id`),
+  ADD CONSTRAINT `editora_id` FOREIGN KEY (`editora_id`) REFERENCES `editora` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
